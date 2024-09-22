9dc727c2bd462553960716800e62ca31f5d22b8b
parthpishte <127310232+parthpishte@users.noreply.github.com>
2024-09-22 23:56:00 +0530

    Update constants.js

--- chatfront/src/utils/constants.js
+++ chatfront/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const HOST=import.meta.env.VITE_SERVER_URL;
+export const HOST='https://chat-app-backend-swart-ten.vercel.app';
 export const AUTH_ROUTES='api/auth';
 export const CONTACT_ROUTES='api/contacts';
 export const SIGNUP_ROUTES=`${AUTH_ROUTES}/signup`;
