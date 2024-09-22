1285aeb375b33cad0479fa83f2c5048d1e02dfad
parthpishte <127310232+parthpishte@users.noreply.github.com>
2024-09-22 20:59:38 +0530

    Update index.js

--- chatback/index.js
+++ chatback/index.js
@@ -17,7 +17,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: [process.env.ORIGIN],
+  //  origin: [process.env.ORIGIN],
+    origin:'*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true
 }));
